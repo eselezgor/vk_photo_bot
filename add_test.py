5d9d00340304d732f8aac79d43e39087e5b46b04
admin <gbgb@gg>
2020-05-02 18:58:30 +0300

    функция добавления теста. Ошибка unable to open database file

--- add_test.py
+++ add_test.py
@@ -1,6 +1,5 @@
 from data import db_session
-db_session.global_init("db/tests.sqlite")
-session = db_session.create_session()
+
 from data.tests import Tests
 
 """Добавление теста в таблицу"""
@@ -9,5 +8,7 @@ new_test = Tests()
 new_test.question = input('Вопрос')
 new_test.answer_choice = input('Варианты ответа через "**"')
 new_test.answer = input('Правильный ответ')
+db_session.global_init("db/tests.sqlite")
+session = db_session.create_session()
 session.add(new_test)
 session.commit()
